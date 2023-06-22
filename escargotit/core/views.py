28d007852973943ce2cde1401cc1833df324de0c
Kliment27 <paskalevkliment@gmail.com>
2023-06-22 22:14:17 +0300

    Add login

--- escargotit/core/views.py
+++ escargotit/core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page
+            return redirect('success')
+        else:
+            # Display an error message
+            error_message = 'Invalid username or password.'
+            return render(request, 'login.html', {'error_message': error_message})
+    
+    return render(request, 'login.html')
