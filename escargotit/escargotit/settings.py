4b10809b9303ac29e7a0ac3a6a3d2105b1712ce8
Kliment27 <paskalevkliment@gmail.com>
2023-06-22 22:03:43 +0300

    Configure settings.py

--- escargotit/escargotit/settings.py
+++ escargotit/escargotit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,12 +26,23 @@ SECRET_KEY = 'django-insecure-2+1xwss*ohv58p-#06u3)6ro#kke(kk3zn94pli=4ipju(i5f&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.coursera-apps.org', 
+    'localhost',
+    '127.0.0.1',
+    '.labs.coursera.org',
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
+    'rest_framework',
+    'core',
+    'bootstrap4',
+    'fontawesomefree',
+    'jquery',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,9 +127,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# specify Login url so that login_required decorators can redirect here
+LOGIN_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
