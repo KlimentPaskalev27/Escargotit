9de6788fe7ee7fd3ce13df3a80fb86014d4d4748
Kliment27 <paskalevkliment@gmail.com>
2023-09-11 18:24:17 +0300

    media folder route so images uploaded there

--- escargotit/escargotit/urls.py
+++ escargotit/escargotit/urls.py
@@ -22,5 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('api/', include('core.urls')),
 ]
+
+# specify how the media folder is routed so that image are uploaded there 
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
